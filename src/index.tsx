e439a74a3e2cf96a6ef2710f08426c8f78bfe5b6
Rodrigo Diniz Goulart <rodrigo.goulart@img.com.br>
2022-11-23 17:54:14 -0300

    Estilização de cardápio e importação de logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/Cardapio/cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>
-      Aluroni
-    </div>
+    <Cardapio />
   </React.StrictMode>
 );
--- src/pages/Cardapio/cardapio.tsx
+++ src/pages/Cardapio/cardapio.tsx
@@ -1,10 +1,11 @@
 import style from './Cardapio.module.scss';
+import {ReactComponent as Logo} from '../../assets/logo.svg';
 
 export default function Cardapio() {
     return(
         <main>
-            <nav>
-
+            <nav className={style.menu}>
+                <Logo />
             </nav>
         </main>
     )
